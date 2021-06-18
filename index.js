5201411375746f71f919858be5e1e591c6df04d2
Wiand1515 <marofual@gmail.com>
2021-06-18 16:17:01 -0400

    First Commit falta arreglar el paso de la seleccion de usuario

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import reactDom from "react-dom";
+
+App = () => {
+    <>
+    <h1>Hola mundo</h1>
+    </>
+}
+
+reactDom.render(<App/>, document.querySelector("#root"));
